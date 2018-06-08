f6d603dd989ae093f13a250542db7b9523168097
janx2379 <40057455+janx2379@users.noreply.github.com>
2018-06-08 14:45:42 +0200

    changes in main

--- src/main.c
+++ src/main.c
@@ -5,7 +5,18 @@
 #include "vectors.h"
 
 int main(void) {
+    //variables
+    uint8_t x1 = 1, y1 = 1, x2 = 100, y2 = 30; // window size
+    x2 = (((x2 - x1 - 1) / 10) * 10) + x1 + 1; // makes the width divisible by 10 (number of boxes in game)
+
+    //initialisation
     init_usb_uart(115200);
-    
-    while(1) {}
+    initJoyStick();
+    initTimer();
+
+
+
+    while(1) {
+
+    }
 }
