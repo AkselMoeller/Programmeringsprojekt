98776934ef04e74d9e90a2c8106bf66fcddedbf8
Alexander <s173904@student.dtu-dk>
2018-06-08 14:56:45 +0200

    main update

--- src/main.c
+++ src/main.c
@@ -4,12 +4,18 @@
 #include "draw_objects.h"
 #include "vectors.h"
 
-int main(void) {
-    //variables
-    uint8_t x1 = 1, y1 = 1, x2 = 100, y2 = 30; // window size
-    x2 = (((x2 - x1 - 1) / 10) * 10) + x1 + 1; // makes the width divisible by 10 (number of boxes in game)
+void gameOver() {
+    
+}
 
-    //initialisation
+void
+
+int main(void) { //Controlling the game
+    //Variables
+    uint8_t x1 = 1, y1 = 1, x2 = 100, y2 = 30; //Window size
+    x2 = (((x2 - x1 - 1) / 10) * 10) + x1 + 1; //Makes the width divisible by 10 (number of boxes in game)
+
+    //initialization
     init_usb_uart(115200);
     initJoyStick();
     initTimer();
@@ -17,6 +23,10 @@ int main(void) {
 
 
     while(1) {
-
+        if (flag) {
+            //Everything here is executed once every 1/60 second
+            
+            flag = 0;
+        }
     }
 }
