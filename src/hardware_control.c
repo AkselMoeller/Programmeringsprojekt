5e9af0bacac9c76b1151913138f1d3bfafe751c8
janx2379 <40057455+janx2379@users.noreply.github.com>
2018-06-14 13:51:26 +0200

    d

--- src/hardware_control.c
+++ src/hardware_control.c
@@ -130,5 +130,5 @@ int8_t readTemperature() {
 
     int16_t x = ADC_GetConversionValue(ADC1); // Read ADC value
 
-    return 338 - x/5; // convert a number from 1690(min temp) - 1650(max temp) to a number from 0-8
+    return 336 - x/5; // convert a number from 1690(min temp) - 1650(max temp) to a number from 0-8
 }
