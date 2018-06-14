de3b80658da7c5a52c110e8c8010d2ba62e6162c
janx2379 <40057455+janx2379@users.noreply.github.com>
2018-06-14 16:43:40 +0200

    PowerUp drop!!!!

--- inc/draw_objects.h
+++ inc/draw_objects.h
@@ -33,7 +33,6 @@ typedef struct {
     uint8_t x, y;
     uint8_t style, hit;
 } powerUp_t;
-void drawPowerUp(powerUp_t powerUp);
 
 typedef struct {
     int32_t x, y;
@@ -42,6 +41,7 @@ typedef struct {
     powerUp_t powerUp;
 } box_t;
 void drawBox(box_t box);
+void drawPowerUp(box_t box);
 
 void drawScoreLabel(uint16_t score, uint8_t x2);
 void drawLevelLabel(uint8_t level, uint8_t x2);
