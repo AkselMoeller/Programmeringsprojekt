05e709c60540416c48998faebdd6ab073abbb052
janx2379 <40057455+janx2379@users.noreply.github.com>
2018-06-15 10:46:26 +0200

    d

--- src/build_game.c
+++ src/build_game.c
@@ -237,7 +237,7 @@ void makeLevel(box_t boxMatrix[MAX_COLUMNS][MAX_ROWS], ball_t * ball_p, striker_
                 case 2 : //LVL 2
                     if (j < 4 && ((j%2 && (i+1)%2) || ((j+1)%2 && i%2))) {
                         boxMatrix[i][j].lives = 1;
-                        if (i == 4){
+                        if (i == 1){
                             boxMatrix[i][j].powerUp.style = 1; // power up for extra ball
                             boxMatrix[i][j].lives = 2;
                         }
