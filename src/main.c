b0a47fe45a97ec48bd49c7c98aeb2c9401a92fc7
Alexander <s173904@student.dtu-dk>
2018-06-18 13:37:15 +0200

    d

--- src/main.c
+++ src/main.c
@@ -203,11 +203,11 @@ int main(void) {
         }
 
         //Writes score to scoreboard when the game has finished and if the score is great enough
-        if (gameIsDone) {
+        if (gameIsDone && score > scoreData[9]) {
             FLASH_Unlock(); // Unlock FLASH for writing
             FLASH_ClearFlag( FLASH_FLAG_EOP | FLASH_FLAG_PGERR | FLASH_FLAG_WRPERR );
             FLASH_ErasePage( address ); // Erase entire page before writing
-            for (int i = 0 ; i < 10 ; i++ ){
+            for (int i = 0 ; i < 10 ; i++ ) {
                 if (score > scoreData[i] && !writtenToScoreboard) {
                     lastVal = scoreData[i];
                     scoreData[i] = score;
